bc8b97889e78a42230476f5647c9b74c522786fa
teerapatFluke <teerapat.choet@outlook.com>
2021-08-20 02:50:36 +0700

    host2

--- kpop_admin/settings.py
+++ kpop_admin/settings.py
@@ -103,7 +103,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILE_DIRS = [
+    "static/images",
+    "static/css",
+    "staticfiles",
+]
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
